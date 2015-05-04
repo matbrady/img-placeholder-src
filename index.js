da9d6528184cbca5b99c4c5c63c97f1f844cdf1f
Mat Brady <mat.brady@threespot.com>
2015-05-04 15:52:54 -0400

    removing unneccssary duplication and adding additional tests

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ var _        = require('underscore');
 var nunjucks = require('nunjucks');
 var srcset   = require('srcset');
 
+var BaseService = {
+  srcset: function(data) {
+    var _this = this;
+    var sources = [];
+
+    data.forEach(function(imageData) {
+      imageData['url'] = _this.src(imageData);
+      sources.push(imageData);
+    });
+    return srcset.stringify(sources);
+  }
+}
+
 var ImagePlaceholderSrc = function(config) {
   var _this = this;
   var defaults = {
@@ -9,10 +22,15 @@ var ImagePlaceholderSrc = function(config) {
   };
 
   /* Placeholder Services */
+  /* 
+    TODO: Override http vs https - note some services may not provide it 
+    see: http://stackoverflow.com/questions/10348906/how-to-know-if-a-request-is-http-or-https-in-node-js
+  */
   var tmpl = {
-    placeholdit: 'http://placehold.it/{{ width }}x{{ height }}{{ "/"+background if background }}{{ "/"+foreground if foreground }}', //<%= '.'+format %>
+    lorempixel:  'http://lorempixel.com{{ "/"+filter if filter }}/{{ width }}/{{ height }}{{ "/"+category if category }}{{ "/"+text if (text and category) }}',
+    placeholdit: 'http://placehold.it/{{ width }}x{{ height }}{{ "/"+background if background }}{{ "/"+foreground if foreground }}{{ "."+format if format }}{{ "&text="+text if text }}', //<%= '.'+format %>
     placeimg:    'http://placeimg.com/{{ width }}/{{ height }}{{ "/"+category if category }}{{ "/"+filter if filter }}',
-    placecage:   'http://placecage.com{{ "/"+mod if mod }}/{{ width }}/{{ height }}',
+    placecage:   'http://placecage.com{{ "/"+filter if filter }}/{{ width }}/{{ height }}',
     fillmurray:  'http://fillmurray.com{{ "/"+filter if filter }}/{{ width }}/{{ height }}'
   };
 
@@ -36,72 +54,88 @@ var ImagePlaceholderSrc = function(config) {
     }
   }
 
-  return {
+  var services = {
 
     placeholdit: {
       src: function(data) {
+        var data = _.clone(data);
         var template = createTemplate(tmpl.placeholdit);
+        if (!!data.text) {
+          data['text'] = data.text.replace(" ", "+");
+        }
         return renderTemplate(template, data);
-      },
-      srcset: function(data) {
-        var _this = this;
-        var sources = [];
-        data.forEach(function(imageData) {
-          imageData['url'] = _this.src(imageData);
-          sources.push(imageData);
-        });
-        return srcset.stringify(sources);
       }
     },
 
-    placeimg: function(data) {
+    lorempixel: {
       src: function(data) {
+        var data = _.clone(data);
+        var template = createTemplate(tmpl.lorempixel);
+        // Image Modifications
+        if (!!data.text) {
+          data['text'] = data.text.replace(" ", "-");
+        }
+        if (!!data.filter) {
+          switch(data.filter) {
+            case 'greyscale':
+              data.filter = 'g';
+              break;
+          }
+        }
+        return renderTemplate(template, data);
+      }
+    },
+
+    placeimg: {
+      src: function(data) {
+        var data = _.clone(data);
+        if (!data.category) {
+          data['category'] = 'any'
+        }
         var template = createTemplate(tmpl.placeimg);
         return renderTemplate(template, data);
-      },
-      srcset: function(data) {
-        var _this = this;
-        var sources = [];
-        data.forEach(function(imageData) {
-          imageData['url'] = _this.src(imageData);
-          sources.push(imageData);
-        });
-        return srcset.stringify(sources);
       }
     },
 
-    placecage: function(data) {
+    placecage: {
       src: function(data) {
+        var data = _.clone(data);
         var template = createTemplate(tmpl.placecage);
+        if (!!data.filter) {
+          switch(data.filter) {
+            case 'greyscale':
+              data.filter = 'g';
+              break;
+            case 'crazy':
+              data.filter = 'c';
+              break;
+          }
+        }
         return renderTemplate(template, data);
-      },
-      srcset: function(data) {
-        var _this = this;
-        var sources = [];
-        data.forEach(function(imageData) {
-          imageData['url'] = _this.src(imageData);
-          sources.push(imageData);
-        });
-        return srcset.stringify(sources);
       }
     },
 
-    fillmurray: function(data) {
+    fillmurray: {
       src: function(data) {
+        var data = _.clone(data);
         var template = createTemplate(tmpl.fillmurray);
+        if (!!data.filter) {
+          switch(data.filter) {
+            case 'greyscale':
+              data.filter = 'g';
+              break;
+          }
+        }
         return renderTemplate(template, data);
-      },
-      srcset: function(data) {
-        var _this = this;
-        var sources = [];
-        data.forEach(function(imageData) {
-          imageData['url'] = _this.src(imageData);
-          sources.push(imageData);
-        });
-        return srcset.stringify(sources);
       }
     }
   }
+
+  _.each(services, function(service, key, list) {
+    _.extend(services[key], BaseService);
+  });
+
+  return services;
 }; 
 
 module.exports = ImagePlaceholderSrc;
--- test/index.js
+++ test/index.js
@@ -4,29 +4,100 @@ var IPS    = require('../index');
 
 var ips = IPS();
 
-var singleImageData = {
+var defaults = {
   "height": 100,
   "width":  100
-  // "filter": "greyscale",
-  // "foreground": "ffffff",
-  // "background": "999999",
-  // "type": "gif",
-  // "text": "Hello world"
+  "filter": "greyscale",
+  "foreground": "ffffff",
+  "background": "999999",
+  "format": "gif",
+  "text": "Hello world",
+  category: "people"
 };
 
-var srcsetImageArray = [
- {
-  height: 100,
-  width: 100
- },
- {
-  height: 200,
-  width: 200
- }
-];
+var testData = {
+  all: {
+    category: {
+      category: "people",
+      height: 100,
+      width: 100
+    },
+    colors: {
+      height: 100,
+      width: 100,
+      foreground: '000000',
+      background: 'dddddd'
+    },
+    filter: {
+      filter: "greyscale",
+      height: 100,
+      width: 100
+    },
+    format: {
+      height: 100, 
+      width: 100, 
+      format: 'jpg' 
+    },
+    src: {
+      height: 100,
+      width: 100
+    },
+    srcset: [
+      {
+        height: 100,
+        width: 100
+      },
+      {
+        height: 200,
+        width: 200
+      }
+    ],
+    text: {
+      height: 100,
+      width: 100,
+      text: "hello world"
+    }
+  },
+  placecage: {
+    crazy: {
+      filter: 'crazy',
+      height: 100,
+      width: 100
+    }
+  }
+};
 
-var srcTest1 = 'http://placehold.it/100x100';
-var srcTest2 = 'http://placehold.it/100x100 100w 100h, http://placehold.it/200x200 200w 200h';
+var results = {
+  placeholdit: {
+    colors:  "http://placehold.it/100x100/dddddd/000000",
+    format: "http://placehold.it/100x100.jpg",
+    src:    "http://placehold.it/100x100",
+    srcset: 'http://placehold.it/100x100 100w 100h, http://placehold.it/200x200 200w 200h',
+    text:   "http://placehold.it/100x100&text=hello+world"
+  },
+  lorempixel: {
+    category: "http://lorempixel.com/100/100/people",
+    filter: "http://lorempixel.com/g/100/100",
+    src: "http://lorempixel.com/100/100",
+    srcset: "http://lorempixel.com/100/100 100w 100h, http://lorempixel.com/200/200 200w 200h"
+  },
+  fillmurray: {
+    filter: "http://fillmurray.com/g/100/100",
+    src: "http://fillmurray.com/100/100",
+    srcset: "http://fillmurray.com/100/100 100w 100h, http://fillmurray.com/200/200 200w 200h"
+  },
+  placecage: {
+    filter: "http://placecage.com/g/100/100",
+    crazy: "http://placecage.com/c/100/100",
+    src: "http://placecage.com/100/100",
+    srcset: "http://placecage.com/100/100 100w 100h, http://placecage.com/200/200 200w 200h"
+  },
+  placeimg: {
+    filter: "http://placeimg.com/100/100/any/greyscale",
+    src: "http://placeimg.com/100/100/any",
+    srcset: "http://placeimg.com/100/100/any 100w 100h, http://placeimg.com/200/200/any 200w 200h"
+  }
+};
 
 describe('#placeholdit', function() {
 
@@ -34,11 +105,114 @@ describe('#placeholdit', function() {
     assert.isFunction(ips.placeholdit.src);
   });
 
-  it('should return a string of "' + srcTest1 + '"', function() {
-    ips.placeholdit.src(singleImageData).should.equal(srcTest1);
+  it('should return a string of "' + testData.all.src + '"', function() {
+    ips.placeholdit.src(testData.all.src).should.equal(results.placeholdit.src);
   });
 
-  it('should return a srcset string of "' + srcTest2 +'"', function() {
-    ips.placeholdit.srcset(srcsetImageArray).should.equal(srcTest2);
+  it('should return a srcset string of "' + results.placeholdit.srcset +'"', function() {
+    ips.placeholdit.srcset(testData.all.srcset).should.equal(results.placeholdit.srcset);
   });
+
+  it('should accept text and return a string of "' + results.placeholdit.text +'"', function() {
+    ips.placeholdit.src(testData.all.text).should.equal(results.placeholdit.text);
+  });
+
+  it('should accept a format and return a string of "' + results.placeholdit.format +'"', function() {
+    ips.placeholdit.src(testData.all.format).should.equal(results.placeholdit.format);
+  });
+
+   it('should accept a colors and return a string of "' + results.placeholdit.colors +'"', function() {
+    ips.placeholdit.src(testData.all.colors).should.equal(results.placeholdit.colors);
+  });
+
 });
+
+describe('#lorempixel', function() {
+
+  it('should have a lorempixel function', function() {
+    assert.isFunction(ips.lorempixel.src);
+  });
+
+  it('should return a string of "' + results.lorempixel.src + '"', function() {
+    ips.lorempixel.src(testData.all.src).should.equal(results.lorempixel.src);
+  });
+
+  it('should return a srcset string of "' + results.lorempixel.srcset +'"', function() {
+    ips.lorempixel.srcset(testData.all.srcset).should.equal(results.lorempixel.srcset);
+  });
+
+  it('should return a filter string of "' + results.lorempixel.filter  +'"', function() {
+    ips.lorempixel.src(testData.all.filter).should.equal(results.lorempixel.filter);
+  });
+
+  it('should return a category string of "' + results.lorempixel.category  +'"', function() {
+    ips.lorempixel.src(testData.all.category).should.equal(results.lorempixel.category);
+  });
+  
+});
+
+describe('#fillmurray', function() {
+
+  it('should have a fillmurray function', function() {
+    assert.isFunction(ips.fillmurray.src);
+  });
+
+  it('should return a string of "' + results.lorempixel.src + '"', function() {
+    ips.fillmurray.src(testData.all.src).should.equal(results.fillmurray.src);
+  });
+
+  it('should return a srcset string of "' + results.lorempixel.srcset +'"', function() {
+    ips.fillmurray.srcset(testData.all.srcset).should.equal(results.fillmurray.srcset);
+  });
+
+  it('should return a filter string of "' + results.fillmurray.filter  +'"', function() {
+    ips.fillmurray.src(testData.all.filter).should.equal(results.fillmurray.filter);
+  });
+  
+});
+
+describe('#placecage', function() {
+
+  it('should have a placecage function', function() {
+    assert.isFunction(ips.placecage.src);
+  });
+
+  it('should return a string of "' + results.placecage.src + '"', function() {
+    ips.placecage.src(testData.all.src).should.equal(results.placecage.src);
+  });
+
+  it('should return a srcset string of "' + results.placecage.srcset +'"', function() {
+    ips.placecage.srcset(testData.all.srcset).should.equal(results.placecage.srcset);
+  });
+
+  it('should return a filter string of "' + results.placecage.filter  +'"', function() {
+    ips.placecage.src(testData.all.filter).should.equal(results.placecage.filter);
+  });
+
+  it('should return a filter string of "' + results.placecage.crazy  +'"', function() {
+    ips.placecage.src(testData.placecage.crazy).should.equal(results.placecage.crazy);
+  });
+  
+});
+
+describe('#placeimg', function() {
+
+  it('should have a placeimg function', function() {
+    assert.isFunction(ips.placeimg.src);
+  });
+
+  it('should return a string of "' + results.placeimg.src + '"', function() {
+    ips.placeimg.src(testData.all.src).should.equal(results.placeimg.src);
+  });
+
+  it('should return a srcset string of "' + results.placeimg.srcset +'"', function() {
+    ips.placeimg.srcset(testData.all.srcset).should.equal(results.placeimg.srcset);
+  });
+
+  it('should return a filter string of "' + results.placeimg.filter  +'"', function() {
+    ips.placeimg.src(testData.all.filter).should.equal(results.placeimg.filter);
+  });
+  
+});
+
+
