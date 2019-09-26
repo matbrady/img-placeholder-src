d31a3ec93ed7932f4a640cd0693d60d69c8b0955
Mat Brady <matbr8dy@gmail.com>
2019-09-26 15:40:59 -0400

    feat: adding www subdomain to urls

--- src/services.es6.js
+++ src/services.es6.js
@@ -1,5 +1,4 @@
 var services = {
-
   /**
    * Placehold.it Service - http://placehold.it/
    * Supports: custom text
@@ -8,22 +7,37 @@ var services = {
   placeholdit: {
     name: 'placeholdit',
     render: function(args) {
-      const { protocol, width, height, background, foreground, format, text } = args;
-      return `${ typeof(protocol) !== 'undefined' ? protocol : ""}`
-        + `//placehold.it/${width}x${height}`
-        + `${ typeof(background) !== 'undefined' ? '/'+background : ""}`
-        + `${ typeof(foreground) !== 'undefined' ? '/'+foreground : ""}`
-        + `${ typeof(format)     !== 'undefined' ? '.'+format     : ""}`
-        + `${ (typeof(text)      !== 'undefined' && typeof(format) === 'undefined') ? '?text='+text : ""}`;
+      const {
+        protocol,
+        width,
+        height,
+        background,
+        foreground,
+        format,
+        text
+      } = args;
+      return (
+        `${typeof protocol !== 'undefined' ? protocol : ''}` +
+        `//www.placehold.it/${width}x${height}` +
+        `${typeof background !== 'undefined' ? '/' + background : ''}` +
+        `${typeof foreground !== 'undefined' ? '/' + foreground : ''}` +
+        `${typeof format !== 'undefined' ? '.' + format : ''}` +
+        `${
+          typeof text !== 'undefined' && typeof format === 'undefined'
+            ? '?text=' + text
+            : ''
+        }`
+      );
     },
     modifier: function(data, options) {
       var formats = ['png', 'gif', 'jpg', 'jpeg'];
       if (!!data.text) {
-        data['text'] = data.text.replace(" ", "+");
+        data['text'] = data.text.replace(' ', '+');
       }
       // Only accept [png, gif, jpg, jpeg] formats
       if (!!data.format) {
-        if (formats.indexOf(data.format) === -1) { // indexOf support: ie9
+        if (formats.indexOf(data.format) === -1) {
+          // indexOf support: ie9
           data.format = undefined;
         }
       }
@@ -40,19 +54,25 @@ var services = {
     name: 'lorempixel',
     render: function(args) {
       const { protocol, filter, width, height, category, text } = args;
-      return `${ typeof(protocol) !== 'undefined' ? protocol : ""}`
-        + `//lorempixel.com`
-        + `${ typeof(filter) !== 'undefined' ? '/'+filter : ""}`
-        + `/${width}/${height}`
-        + `${ typeof(category) !== 'undefined' ? '/'+category : ""}`
-        + `${ (typeof(text) !== 'undefined' && typeof(category) !== 'undefined') ? '/'+text : ""}`;
+      return (
+        `${typeof protocol !== 'undefined' ? protocol : ''}` +
+        `//www.lorempixel.com` +
+        `${typeof filter !== 'undefined' ? '/' + filter : ''}` +
+        `/${width}/${height}` +
+        `${typeof category !== 'undefined' ? '/' + category : ''}` +
+        `${
+          typeof text !== 'undefined' && typeof category !== 'undefined'
+            ? '/' + text
+            : ''
+        }`
+      );
     },
     modifier: function(data, options) {
       if (!!data.text) {
-        data['text'] = data.text.replace(" ", "-");
+        data['text'] = data.text.replace(' ', '-');
       }
       if (!!data.filter) {
-        switch(data.filter) {
+        switch (data.filter) {
           case 'greyscale':
             data.filter = 'g';
             break;
@@ -71,28 +91,29 @@ var services = {
     name: 'placeimg',
     render: function(args) {
       const { protocol, width, height, category, filter } = args;
-      return `${ typeof(protocol) !== 'undefined' ? protocol : ""}`
-        + `//placeimg.com/${width}/${height}`
-        + `${ typeof(category) !== 'undefined' ? '/'+category : ""}`
-        + `${ typeof(filter) !== 'undefined' ? '/'+filter : ""}`;
+      return (
+        `${typeof protocol !== 'undefined' ? protocol : ''}` +
+        `//www.placeimg.com/${width}/${height}` +
+        `${typeof category !== 'undefined' ? '/' + category : ''}` +
+        `${typeof filter !== 'undefined' ? '/' + filter : ''}`
+      );
     },
     modifier: function(data, options) {
       var filters = ['greyscale', 'grayscale', 'sepia'];
       if (!data.category) {
-        data['category'] = 'any'
+        data['category'] = 'any';
       }
       if (!!data.filter) {
         if (filters.indexOf(data.filter) === -1) {
           data.filter = undefined;
-        }
-        else {
-          switch(data.filter) {
+        } else {
+          switch (data.filter) {
             case 'greyscale':
-            data.filter = 'grayscale';
-            break;
+              data.filter = 'grayscale';
+              break;
             case 'sepia':
-            data.filter = 'sepia';
-            break;
+              data.filter = 'sepia';
+              break;
           }
         }
       }
@@ -109,16 +130,22 @@ var services = {
     name: 'placecage',
     render: function(args) {
       const { protocol, width, height, filter, format } = args;
-      return `${ typeof(protocol) !== 'undefined' ? protocol : ""}`
-        + `//placecage.com`
-        + `${ typeof(format) !== 'undefined' ? '/'+format : ""}`
-        + `${ typeof(filter) !== 'undefined' && typeof(format) === 'undefined' ? '/'+filter : ""}`
-        + `/${width}/${height}`;
+      return (
+        `${typeof protocol !== 'undefined' ? protocol : ''}` +
+        `//www.placecage.com` +
+        `${typeof format !== 'undefined' ? '/' + format : ''}` +
+        `${
+          typeof filter !== 'undefined' && typeof format === 'undefined'
+            ? '/' + filter
+            : ''
+        }` +
+        `/${width}/${height}`
+      );
     },
     modifier: function(data, options) {
       var formats = ['gif'];
       if (!!data.filter) {
-        switch(data.filter) {
+        switch (data.filter) {
           case 'greyscale':
             data.filter = 'g';
             break;
@@ -149,14 +176,16 @@ var services = {
     name: 'fillmurray',
     render: function(args) {
       const { protocol, width, height, filter } = args;
-      return `${ typeof(protocol) !== 'undefined' ? protocol : ""}`
-        + `//fillmurray.com`
-        + `${ typeof(filter) !== 'undefined' ? '/'+filter : ""}`
-        + `/${width}/${height}`;
+      return (
+        `${typeof protocol !== 'undefined' ? protocol : ''}` +
+        `//www.fillmurray.com` +
+        `${typeof filter !== 'undefined' ? '/' + filter : ''}` +
+        `/${width}/${height}`
+      );
     },
     modifier: function(data, options) {
       if (!!data.filter) {
-        switch(data.filter) {
+        switch (data.filter) {
           case 'greyscale':
             data.filter = 'g';
             break;
@@ -177,28 +206,39 @@ var services = {
   satyr: {
     name: 'satyr',
     render: function(args) {
-      const { protocol, width, height, background, theme, format, text, delay, brand } = args;
+      const {
+        protocol,
+        width,
+        height,
+        background,
+        theme,
+        format,
+        text,
+        delay,
+        brand
+      } = args;
       const params = ['format', 'text', 'delay', 'brand', 'flag', 'texture'];
-      let result = "";
+      let result = '';
 
-      result = `${ typeof(protocol) !== 'undefined' ? protocol : ""}`
-        + `//satyr.io/${width}x${height}`
-        + `${ typeof(background) !== 'undefined' ? '/'+background     : ""}`
-        + `${ typeof(theme)      !== 'undefined' ? '/'+theme          : ""}`;
+      result =
+        `${typeof protocol !== 'undefined' ? protocol : ''}` +
+        `//www.satyr.io/${width}x${height}` +
+        `${typeof background !== 'undefined' ? '/' + background : ''}` +
+        `${typeof theme !== 'undefined' ? '/' + theme : ''}`;
 
-        // check for any parameter variables and set flag
-        let count = 1;
-        params.forEach(function(param, index) {
-          if (args.hasOwnProperty(param)) {
-            let value = args[param];
-            // change `format` to `type`
-            if (param === 'format') {
-              param = 'type';
-            }
-            result += `${(count <= 1 ? '?' : '&')}${param}=${value}`
-            count++
+      // check for any parameter variables and set flag
+      let count = 1;
+      params.forEach(function(param, index) {
+        if (args.hasOwnProperty(param)) {
+          let value = args[param];
+          // change `format` to `type`
+          if (param === 'format') {
+            param = 'type';
           }
-        })
+          result += `${count <= 1 ? '?' : '&'}${param}=${value}`;
+          count++;
+        }
+      });
 
       return result;
     },
@@ -206,16 +246,17 @@ var services = {
       // Only accept [webp, png, gif, jpg, jpeg] formats
       var formats = ['webp', 'png', 'gif', 'jpg', 'jpeg'];
       if (!!data.text) {
-        data['text'] = data.text.replace(" ", "+");
+        data['text'] = data.text.replace(' ', '+');
       }
       if (!!data.format) {
-        if (formats.indexOf(data.format) === -1) { // indexOf support: ie9
+        if (formats.indexOf(data.format) === -1) {
+          // indexOf support: ie9
           data.format = undefined;
         }
       }
       return data;
     }
-  },
+  }
 };
 
 module.exports = services;
